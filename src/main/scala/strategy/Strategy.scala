c2a3b4e53174767cb3804b0091e9604c99e6c044
Niklas Deworetzki <niklas-deworetzki@t-online.de>
2021-11-26 15:20:49 +0100

    Implemented new Strategy.

--- src/main/scala/strategy/Strategy.scala
+++ src/main/scala/strategy/Strategy.scala
@@ -3,7 +3,7 @@ package strategy
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait Strategy extends Runnable {
+trait Strategy {
   type Preprocessed
   type Parsed1
   type Parsed2
@@ -38,6 +38,22 @@ object Strategy {
     }
   }
 
+  trait Shared extends Strategy {
+    type Parsed
+    override type Parsed1 = Parsed
+    override type Parsed2 = Parsed
+
+    def parse(input: Preprocessed): Parsed
+
+    def solve1(input: Parsed): Solution1
+    def solve2(input: Parsed): Solution2
+
+    override def run(input: String): Unit = {
+      val parsed = parse(preprocess(input))
+      println(solve1(parsed))
+      println(solve2(parsed))
+    }
+  }
 
   trait Incrementing extends Strategy {
     override type Parsed2 = Nothing
@@ -55,13 +71,7 @@ object Strategy {
     }
   }
 
-  trait Parallel extends Strategy {
-    def parse1(input: Preprocessed): Parsed1
-    def solve1(input: Parsed1): Solution1
-
-    def parse2(input: Preprocessed): Parsed2
-    def solve2(input: Parsed2): Solution2
-
+  trait Parallel extends Default {
     implicit private val executionContext: ExecutionContext = ExecutionContext.global
 
     override def run(input: String): Unit = {
@@ -72,4 +82,14 @@ object Strategy {
     }
   }
 
+  trait ParallelShared extends Shared {
+    implicit private val executionContext: ExecutionContext = ExecutionContext.global
+
+    override def run(input: String): Unit = {
+      val parsed = parse(preprocess(input))
+      val solution2 = Future[Solution2](solve2(parsed))
+      println(solve1(parsed))
+      solution2.onComplete(_.foreach(println))
+    }
+  }
 }
