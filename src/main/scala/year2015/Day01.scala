f06124955304ac0d599febfd67a5c57badd42f31
Niklas Deworetzki <niklas-deworetzki@t-online.de>
2021-11-26 15:21:00 +0100

    Implemented solution for first day.

---
+++ src/main/scala/year2015/Day01.scala
@@ -0,0 +1,19 @@
+package aoc
+package aoc.year2015
+
+import strategy.Strategy
+import strategy.Strategy.NoPreprocessing
+
+object Day01 extends Day(1) with Strategy.Shared with NoPreprocessing {
+  override type Parsed = Iterable[Int]
+  override type Solution1 = Int
+  override type Solution2 = Int
+
+  def parse(input: Preprocessed): Parsed = input.map {
+    case '(' => +1
+    case ')' => -1
+  }
+
+  def solve1(input: Parsed): Solution1 = input.sum
+  def solve2(input: Parsed): Solution2 = input.scanLeft(0)(_ + _).takeWhile(_ >= 0).size
+}
