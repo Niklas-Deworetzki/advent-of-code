1b368c6d8172564ba6a02122e64abd2894ef01e1
Niklas Deworetzki <niklas-deworetzki@t-online.de>
2021-12-07 06:08:55 +0100

    Implemented Solution for Part 7.

---
+++ src/main/scala/aoc/year2021/Day07.scala
@@ -0,0 +1,29 @@
+package aoc.year2021
+
+import aoc.Day
+import aoc.strategy.Strategy
+
+object Day07 extends Day with Strategy.Shared {
+  override type Preprocessed = List[String]
+  override type Parsed = List[Int]
+
+  override def preprocess(input: String): Preprocessed = input.split(',').toList
+
+  override def parse(input: Preprocessed): Parsed = input.map(_.toInt)
+
+  override type Solution1 = Long
+  override type Solution2 = Long
+
+  override def solve1(input: Parsed): Solution1 = {
+    for (i <- input.min to input.max)
+      yield input.map(_ - i).map(Math.abs).sum
+  }.min
+
+  override def solve2(input: Parsed): Solution2 = {
+    for (i <- input.min to input.max)
+      yield input.map(crabDifference(i)).map(Math.abs).sum
+  }.min
+
+  def crabDifference(target: Int)(origin: Int): Int =
+    (1 to (Math.abs(target - origin))).sum
+}
